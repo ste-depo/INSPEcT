a1332515d77da65f574f56a2175d829f55faf692
ste-depo <ste.depo@gmail.com>
2019-02-11 12:51:22 +0100

    modified simulated data and updated NEWS file

---
+++ inst/convert_ids.R
@@ -0,0 +1,25 @@
+convert_ids = function(ids, degDuringPulse=FALSE) {
+
+	## rearrange ratesFirstGuess and ratesFirstGuessVar
+	fgVar= fData(ids@ratesFirstGuess)
+	fgVar= fgVar[, grep('t0',colnames(fgVar),invert=TRUE)]
+	totVar= sapply(seq_along(ids@tpts), function(i) fgVar[,grep('total', colnames(fgVar))])
+	preVar= sapply(seq_along(ids@tpts), function(i) fgVar[,grep('preMRNA', colnames(fgVar))])
+	synVar= sapply(seq_along(ids@tpts), function(i) fgVar[,grep('synthesis', colnames(fgVar))])
+	proVar <- degVar <- matrix(1, nrow(synVar), ncol(synVar))
+	varData = cbind(totVar, preVar, synVar, proVar, degVar)
+	pData <- phenoData(ids@ratesFirstGuess)
+	colnames(varData) = rownames(pData@data)
+	rownames(varData) = featureNames(ids@ratesFirstGuess)
+	ids@ratesFirstGuess <- ExpressionSet(assayData=exprs(ids@ratesFirstGuess), phenoData=pData)
+	ids@ratesFirstGuessVar <- ExpressionSet(assayData=varData, phenoData=pData)
+
+	## add new slots
+	ids@NoNascent <- FALSE
+	ids@degDuringPulse <- degDuringPulse[1]
+	ids@model@params$preferPValue <- TRUE
+	ids@model@params$padjG <- TRUE
+
+	return(ids)
+
+}
